9a375f5b1a12b27a4d165c8c4cccbd231269ad77
zeikomi552 <68983834+zeikomi552@users.noreply.github.com>
2021-04-14 07:18:56 +0900

    行動一覧の取得処理を作成

---
+++ Destinationboard/Models/ActionPlanM.cs
@@ -0,0 +1,66 @@
+﻿using Destinationboard.Common.Utilities;
+using Destinationboard.Models.db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Destinationboard.Models
+{
+    public class ActionPlanM : ActionPlanTableBase
+    {
+        #region コンストラクタ
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public ActionPlanM()
+		{
+
+		}
+        #endregion
+
+        #region コンストラクタ
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="reply">リプライ</param>
+        public ActionPlanM(ActionPlanTableReply reply)
+        {
+            this.Copy(reply);
+        }
+        #endregion
+
+        #region Copy処理(ActionPlanTableReplyを変換する)
+        /// <summary>
+        /// Copy処理(ActionPlanTableReplyを変換する)
+        /// </summary>
+        /// <param name="reply">ActionPlanTableReply</param>
+        public void Copy(ActionPlanTableReply reply)
+        {
+            this.ActionID = reply.ActionID;                 // 行動ID
+            this.ActionName = reply.ActionName;             // 行動名
+            this.DestinationID = reply.DestinationID;       // 行先ID
+            this.DestinationName = reply.DestinationName;   // 行先名
+
+            this.FromTime = null;
+            // 開始時刻のnullチェック
+            if (!reply.FromTime.Equals(string.Empty))
+            {
+                // 開始時刻をセット
+                this.FromTime = DateTime.ParseExact(reply.FromTime, "yyyy/MM/dd HH:mm:ss", null);
+            }
+            this.ToTime = null;
+            // 終了時刻のnullチェック
+            if (!reply.ToTime.Equals(string.Empty))
+            {
+                // 終了時刻をセット
+                this.ToTime = DateTime.ParseExact(reply.ToTime, "yyyy/MM/dd HH:mm:ss", null);
+            }
+
+            // メモのセット
+            this.Memo = reply.Memo;
+        }
+        #endregion
+    }
+}
--- Destinationboard/ViewModels/MainWindowVM.cs
+++ Destinationboard/ViewModels/MainWindowVM.cs
@@ -1,5 +1,8 @@
-﻿using Destinationboard.Common.Utilities;
+﻿using Destinationboard.Common;
+using Destinationboard.Common.Utilities;
+using Destinationboard.Models;
 using Destinationboard.Views;
+using Grpc.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +13,32 @@ namespace Destinationboard.ViewModels
 {
     public class MainWindowVM : ViewModelBase
     {
+        #region 行動予定一覧[ActionPlans]プロパティ
+        /// <summary>
+        /// 行動予定一覧[ActionPlans]プロパティ用変数
+        /// </summary>
+        ActionPlanCollectionM _ActionPlans = new ActionPlanCollectionM();
+        /// <summary>
+        /// 行動予定一覧[ActionPlans]プロパティ
+        /// </summary>
+        public ActionPlanCollectionM ActionPlans
+        {
+            get
+            {
+                return _ActionPlans;
+            }
+            set
+            {
+                if (_ActionPlans == null || !_ActionPlans.Equals(value))
+                {
+                    _ActionPlans = value;
+                    NotifyPropertyChanged("ActionPlans");
+                }
+            }
+        }
+        #endregion
+
+
         #region 初期化処理
         /// <summary>
         /// 初期化処理
@@ -18,10 +47,62 @@ namespace Destinationboard.ViewModels
         {
             try
             {
+                // チャネルの作成
+                var channel = new Grpc.Core.Channel(CommonValues.GetInstance().ServerName,
+                    CommonValues.GetInstance().Port, ChannelCredentials.Insecure);
+
+                // クライアントの作成
+                var client = new DestinationbardCommunicationAPI.DestinationbardCommunicationAPIClient(channel);
+
+                // リクエストの作成
+                GetActionPlansRequest tmp = new GetActionPlansRequest();
+                GetStaffsRequest staff_tmp = new GetStaffsRequest();
+
+                // データの取得
+                var reply = client.GetActionPlans(tmp);
+                var staff_reply = client.GetStaffs(staff_tmp);
+
+                // データの移し替え
+                ActionPlanCollectionM action_list = new ActionPlanCollectionM();
+
+                var staffinf_list = (from x in staff_reply.StaffInfoList
+                                     where x.Display.Equals(true)
+                                     orderby x.SortOrder
+                                     select x);
+
+                // スタッフマスターの情報をベースに行動一覧を作成
+                foreach (var staff in staffinf_list)
+                {
+                    // スタッフマスターの情報の該当する
+                    // 行動情報を取得する
+                    var staff_action = (from x in reply.ActionPlans
+                                      where x.StaffID.Equals(staff.StaffID)
+                                      select x).FirstOrDefault();
+
+                    // 行動情報が見つかった場合
+                    if (staff_action != null)
+                    {
+                        // その行動情報を登録する
+                        action_list.Add(new ActionPlanM(staff_action));
+                    }
+                    else
+                    {
+                        // 空の行動情報を用意する（従業員IDと従業員名のみ）
+                        var emply_action_plan = new ActionPlanM();
+                        emply_action_plan.StaffID = staff.StaffID;
+                        emply_action_plan.StaffName = staff.StaffName;
+                        action_list.Add(emply_action_plan);
+                    }
+                }
+
+                // 画面に表示
+                this.ActionPlans = action_list;
+
             }
             catch (Exception e)
             {
-                _logger.Error("Fatal error", e);
+                _logger.Error("Fatal Error", e);
+                ShowMessage.ShowErrorOK(e.Message, "Error");
             }
         }
         #endregion
